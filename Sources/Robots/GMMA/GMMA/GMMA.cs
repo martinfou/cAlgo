60101ad18282cc5a76d6cb8d3e71e541f897778d
Martin Fournier <martinfou@gmail.com>
2018-04-15 08:35:49 -0400

    refactoring. Using arrays instead of multiple variables for emm

--- Sources/Robots/GMMA/GMMA/GMMA.cs
+++ Sources/Robots/GMMA/GMMA/GMMA.cs
@@ -10,79 +10,34 @@ namespace cAlgo
     [Robot(TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
     public class GMMA : Robot
     {
-        [Parameter(DefaultValue = 0.0)]
-        public double Parameter { get; set; }
-
-        [Output("Short EMA1", Color = Colors.Blue)]
-        public IndicatorDataSeries ShortEma1 { get; set; }
-
-        [Output("Short EMA2", Color = Colors.Blue)]
-        public IndicatorDataSeries ShortEma2 { get; set; }
-
-        [Output("Short EMA3", Color = Colors.Blue)]
-        public IndicatorDataSeries ShortEma3 { get; set; }
-
-        [Output("Short EMA4", Color = Colors.Blue)]
-        public IndicatorDataSeries ShortEma4 { get; set; }
-
-        [Output("Short EMA5", Color = Colors.Blue)]
-        public IndicatorDataSeries ShortEma5 { get; set; }
-
-        [Output("Short EMA6", Color = Colors.Blue)]
-        public IndicatorDataSeries ShortEma6 { get; set; }
-
-        private ExponentialMovingAverage m_shortEma1;
-        private ExponentialMovingAverage m_shortEma2;
-        private ExponentialMovingAverage m_shortEma3;
-        private ExponentialMovingAverage m_shortEma4;
-        private ExponentialMovingAverage m_shortEma5;
-        private ExponentialMovingAverage m_shortEma6;
-
-        private ExponentialMovingAverage m_longEma1;
-        private ExponentialMovingAverage m_longEma2;
-        private ExponentialMovingAverage m_longEma3;
-        private ExponentialMovingAverage m_longEma4;
-        private ExponentialMovingAverage m_longEma5;
-        private ExponentialMovingAverage m_longEma6;
-
+        private ExponentialMovingAverage[] fastEma = new ExponentialMovingAverage[6];
+        private ExponentialMovingAverage[] slowEma = new ExponentialMovingAverage[6];
 
         protected override void OnStart()
         {
-            m_shortEma1 = Indicators.ExponentialMovingAverage(MarketSeries.Close, 3);
-            m_shortEma2 = Indicators.ExponentialMovingAverage(MarketSeries.Close, 5);
-            m_shortEma3 = Indicators.ExponentialMovingAverage(MarketSeries.Close, 8);
-            m_shortEma4 = Indicators.ExponentialMovingAverage(MarketSeries.Close, 10);
-            m_shortEma5 = Indicators.ExponentialMovingAverage(MarketSeries.Close, 12);
-            m_shortEma6 = Indicators.ExponentialMovingAverage(MarketSeries.Close, 15);
+            fastEma[0] = Indicators.ExponentialMovingAverage(MarketSeries.Close, 3);
+            fastEma[1] = Indicators.ExponentialMovingAverage(MarketSeries.Close, 5);
+            fastEma[2] = Indicators.ExponentialMovingAverage(MarketSeries.Close, 8);
+            fastEma[3] = Indicators.ExponentialMovingAverage(MarketSeries.Close, 10);
+            fastEma[4] = Indicators.ExponentialMovingAverage(MarketSeries.Close, 12);
+            fastEma[5] = Indicators.ExponentialMovingAverage(MarketSeries.Close, 15);
 
-            m_longEma1 = Indicators.ExponentialMovingAverage(MarketSeries.Close, 30);
-            m_longEma2 = Indicators.ExponentialMovingAverage(MarketSeries.Close, 35);
-            m_longEma3 = Indicators.ExponentialMovingAverage(MarketSeries.Close, 40);
-            m_longEma4 = Indicators.ExponentialMovingAverage(MarketSeries.Close, 45);
-            m_longEma5 = Indicators.ExponentialMovingAverage(MarketSeries.Close, 50);
-            m_longEma6 = Indicators.ExponentialMovingAverage(MarketSeries.Close, 60);
+            slowEma[0] = Indicators.ExponentialMovingAverage(MarketSeries.Close, 30);
+            slowEma[1] = Indicators.ExponentialMovingAverage(MarketSeries.Close, 35);
+            slowEma[2] = Indicators.ExponentialMovingAverage(MarketSeries.Close, 40);
+            slowEma[3] = Indicators.ExponentialMovingAverage(MarketSeries.Close, 45);
+            slowEma[4] = Indicators.ExponentialMovingAverage(MarketSeries.Close, 50);
+            slowEma[5] = Indicators.ExponentialMovingAverage(MarketSeries.Close, 60);
 
 
         }
 
         protected override void OnBar()
         {
-            m_shortEma1 = Indicators.ExponentialMovingAverage(MarketSeries.Close, 3);
-            m_shortEma2 = Indicators.ExponentialMovingAverage(MarketSeries.Close, 5);
-            m_shortEma3 = Indicators.ExponentialMovingAverage(MarketSeries.Close, 8);
-            m_shortEma4 = Indicators.ExponentialMovingAverage(MarketSeries.Close, 10);
-            m_shortEma5 = Indicators.ExponentialMovingAverage(MarketSeries.Close, 12);
-            m_shortEma6 = Indicators.ExponentialMovingAverage(MarketSeries.Close, 15);
 
-            m_longEma1 = Indicators.ExponentialMovingAverage(MarketSeries.Close, 30);
-            m_longEma2 = Indicators.ExponentialMovingAverage(MarketSeries.Close, 35);
-            m_longEma3 = Indicators.ExponentialMovingAverage(MarketSeries.Close, 40);
-            m_longEma4 = Indicators.ExponentialMovingAverage(MarketSeries.Close, 45);
-            m_longEma5 = Indicators.ExponentialMovingAverage(MarketSeries.Close, 50);
-            m_longEma6 = Indicators.ExponentialMovingAverage(MarketSeries.Close, 60);
-
-            if (isSignalDetected() == true && isSignalConfirmed() == true)
+            if (isLongSignalDetected() == true && isSignalConfirmed() == true)
             {
+                Print("==> longsignal detected");
             }
         }
 
@@ -91,13 +46,27 @@ namespace cAlgo
             // Put your deinitialization logic here
         }
 
-        private bool isSignalDetected()
+        private bool isLongSignalDetected()
         {
-            if (m_shortEma1.Result.HasCrossedBelow(m_longEma6.Result, 0))
+            bool fastEmaLongOrder = true;
+            for (int i = 0; i < fastEma.Length - 1; i++)
             {
-                Print("Short crossed below Long");
+                if (fastEma[i].Result.LastValue < fastEma[i + 1].Result.LastValue)
+                {
+                    fastEmaLongOrder = false;
+                }
             }
-            return true;
+
+            bool slowEmaLongOrder = true;
+            for (int i = 0; i < fastEma.Length - 1; i++)
+            {
+                if (slowEma[i].Result.LastValue > slowEma[i + 1].Result.LastValue)
+                {
+                    slowEmaLongOrder = false;
+                }
+            }
+
+            return (fastEmaLongOrder && slowEmaLongOrder);
         }
 
         private bool isSignalConfirmed()
